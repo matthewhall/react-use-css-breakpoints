158e3676b01877673c5a31d4e82b30b2d2df6e75
Matt Hall <bizear@gmail.com>
2019-10-15 11:10:37 -0700

    Adds in linting and Prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': ['error', 'always']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
